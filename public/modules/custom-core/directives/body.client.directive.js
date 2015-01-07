46ea502f00fec067410d70249da7d4fbc73e8c86
Warren <w.green@hotmail.com>
2015-01-07 20:07:51 +0000

    Updated body with directive for reacting to about state

--- public/modules/custom-core/directives/body.client.directive.js
+++ public/modules/custom-core/directives/body.client.directive.js
@@ -1,15 +1,31 @@
 'use strict';
 
-var body = function body(Authentication) {
+var body = function body($location, Authentication) {
 
   return {
     restrict : 'A',
     replace : false,
     link : function link(scope, elem, attr) {
+      var aboutRegex;
+      var currentUrl;
+      scope.aboutState = false;
+
       scope.authentication = Authentication;
+      aboutRegex = new RegExp('/about');
+
+      function checkState() {
+        currentUrl = $location.url();
+        scope.aboutState = aboutRegex.test(currentUrl);
+
+      }
+
+      scope.$on('$stateChangeSuccess', function() {
+        checkState();
+      })
+
     }
   };
 
 };
 
-angular.module('customCore').directive('body', ['Authentication', body]);
+angular.module('customCore').directive('body', ['$location','Authentication', body]);
